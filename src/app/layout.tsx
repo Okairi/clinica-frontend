187ed75359ae7e3e2fbc918654d0ab9041843d50
Alessandro Marino <alessandroretuertonichi@gmail.com>
2025-10-07 20:30:30 -0500

    feat:add layout for pages

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,21 +11,25 @@ export default function RootLayout({
   return (
     <html lang="es">
       <body>
-        {/* HEADER GLOBAL */}
         <header className="flex items-center justify-between px-8 py-4">
-          <div className="text-2xl font-bold text-red-500">MiLogo</div>
+          <Link href={"/"} className="text-2xl font-bold text-red-500">MiLogo</Link>
           <nav className="flex gap-6">
             <Link href="/">Inicio</Link>
             <Link href="/login">Servicios</Link>
             <Link href="/registro">Doctores</Link>
             <Link href="/contacto">Contacto</Link>
           </nav>
+
+          <div>
+
+
+          </div>
         </header>
 
 
         <main className="p-8">{children}</main>
 
-        <footer className="text-center py-4 text-sm text-gray-500 border-t mt-8">
+        <footer className="text-center py-4 text-sm text-gray-500 border-t mt-8 absolute bottom-0 w-full">
           © {new Date().getFullYear()} Mi App. Todos los derechos reservados.
         </footer>
       </body>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,11 @@
 'use client'
-import { createClient } from '@/utils/supabase/client'
 
 export default function Home() {
-  const supabase = createClient()
 
-  async function probarConexion() {
-    const { data, error } = await supabase.from('doctores').select('*')
-    console.log('Datos:', data, 'Error:', error)
-  }
 
   return (
-    <main>
-      <h1>Clínica Supabase</h1>
-      <button onClick={probarConexion}>Probar conexión</button>
-    </main>
+
+
+    <></>
   )
 }
